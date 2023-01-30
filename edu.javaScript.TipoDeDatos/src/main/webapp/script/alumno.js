bd3d439babca3fb0d4df8a2bfd9c5cab05cd9edb
aaronsx <senen.00@gmail.com>
2023-01-30 09:04:41 +0100

    no message

--- edu.javaScript.TipoDeDatos/src/main/webapp/script/alumno.js
+++ edu.javaScript.TipoDeDatos/src/main/webapp/script/alumno.js
@@ -8,10 +8,32 @@ function Objeto()
 	    	 }
 	    var Alumnos = new alumno(" Aaron Senen", " Munoz Prada", " "+21);
 	
-	    document.write(Alumnos instanceof Object);
-	    document.write(Alumnos);
+	    document.write(typeof(Alumnos));
+	    document.write(typeof(Alumnos) == 'object'); //o document.write(AlumnoUno instanceof Object);
+	    document.write(Alumnos instanceof alumno);
 	    document.write(Alumnos.nombre);
 		document.write(Alumnos.apellido);
 		document.write(Alumnos.edad);
 	}
+	/*CORRECCION 
+	
+		function Objeto()
+		{
+			function alumno(nombre, apellidos, edad)
+			{
+				this.nombre=nombre Si no se pone sin this da un error porque fuera de esa funcion ya o existe
+				this.apellidos=apellidos;
+				this.edad=edad;
+			}
+				var AlumnoUno=new alumno('Regino','Fdez',31);
+				
+				document.write(typeof(AlumnoUno));
+				document.write(AlumnoUno instanceof Object);
+	    		document.write(AlumnoUno instanceof alumno);
+	    		document.write(AlumnoUno.nombre);
+				document.write(AlumnoUno.apellidos);
+				document.write(AlumnoUno.edad);
+		}
+	
+	*/ 
 	
